92b1606e71d6959ba4b66b89c94fce4efab00dc5
Victor <147107766+Crosby01@users.noreply.github.com>
2024-11-10 21:40:34 +0100

    Add files via upload

---
+++ WordCountTool/src/main/java/com/mycompany/wordcounttool/WordCountTool.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.wordcounttool;
+
+/**
+ *
+ * @author Victor
+ */
+public class WordCountTool {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
